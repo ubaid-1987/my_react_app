5ca50514f1bb6e1ba193d6cda52085c5eb03bb48
ubaid-1987 <65660481+ubaid-1987@users.noreply.github.com>
2020-06-08 14:10:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Hello World by reatc app</h1>
+      <h1> Hello World by reatc app after changes</h1>
     </div>
   );
 }
